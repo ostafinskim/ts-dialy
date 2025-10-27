be29ef51e9875fdd23d628dac3e97f5fa020b484
Miro Ostafinski <ostafinskim@gmail.com>
2025-10-27 14:39:52 +0000

    linear search

---
+++ src/search/linear-search.test.ts
@@ -0,0 +1,12 @@
+import linear_search from "./linear-search";
+
+test("linear search array", function () {
+
+	const foo = [1, 3, 4, 69, 71, 81, 90, 99, 420, 1337, 69420];
+	expect(linear_search(foo, 69)).toEqual(true);
+	expect(linear_search(foo, 1336)).toEqual(false);
+	expect(linear_search(foo, 69420)).toEqual(true);
+	expect(linear_search(foo, 69421)).toEqual(false);
+	expect(linear_search(foo, 1)).toEqual(true);
+	expect(linear_search(foo, 0)).toEqual(false);
+});
